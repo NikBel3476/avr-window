4cd12104c5c15b80d53436a72d20306a3a7fbbc7
NikBel3476 <nikita.belkin3476@gmail.com>
2022-09-28 18:38:46 +0400

    chore(deps): update dependencies

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,20 @@ use avr_delay::delay_ms;
 
 #[no_mangle]
 pub extern fn main() {
-		unsafe {
-				// Set the upper four physical pins on PORT B to inputs, the lower four to outputs.
-				// The AVR interprets '1' in the data direction register as 'output', '0' input
-				// for the corresponding pin.
-				write_volatile(DDRE, read_volatile(DDRE) | 0b11111111);
+	unsafe {
+		// Set the upper four physical pins on PORT B to inputs, the lower four to outputs.
+		// The AVR interprets '1' in the data direction register as 'output', '0' input
+		// for the corresponding pin.
+		write_volatile(DDRE, read_volatile(DDRE) | 0b11111111);
 
-				// Write the output pins.
-				write_volatile(PORTE, 0b0);
+		// Write the output pins.
+		write_volatile(PORTE, 0b0);
 
-				loop {
-						write_volatile(PORTE, 0b00000001);
-						delay_ms(1000);
-						write_volatile(PORTE, 0b00000000);
-						delay_ms(1000);
-				}
+		loop {
+			write_volatile(PORTE, 0b00000001);
+			delay_ms(1000);
+			write_volatile(PORTE, 0b00000000);
+			delay_ms(1000);
 		}
+	}
 }
