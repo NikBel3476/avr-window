fc170a051df3678bb5259acbefa50f4fcd66295b
NikBel3476 <nikita.belkin3476@gmail.com>
2022-10-01 14:32:22 +0400

    feature(deps): replace definitions to avrd crate

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,29 @@
 
 extern crate avr_std_stub;
 
-use definitions::atmega128rfa1::{DDRE, PORTE};
+use avrd::atmega128rfa1::{
+	DDRE,
+	PORTE,
+	SREG,
+	TCCR1A,
+	TCCR1B,
+	OCR1A,
+	TCNT1
+};
 use core::ptr::{read_volatile, write_volatile};
-use avr_delay::delay_ms;
 
 #[no_mangle]
 pub extern fn main() {
 	unsafe {
-		// Set the upper four physical pins on PORT B to inputs, the lower four to outputs.
-		// The AVR interprets '1' in the data direction register as 'output', '0' input
-		// for the corresponding pin.
+		write_volatile(SREG, !(1 << 7));
 		write_volatile(DDRE, read_volatile(DDRE) | 0b11111111);
+		write_volatile(PORTE, 0);
 
-		// Write the output pins.
-		write_volatile(PORTE, 0b0);
+		write_volatile(TCCR1A, 0);
 
 		loop {
 			write_volatile(PORTE, 0b00000001);
-			delay_ms(1000);
 			write_volatile(PORTE, 0b00000000);
-			delay_ms(1000);
 		}
 	}
 }
