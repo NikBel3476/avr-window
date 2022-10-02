efdfa3a781e59c696aac6d6a7e1615a8a7ce9193
NikBel3476 <nikita.belkin3476@gmail.com>
2022-10-02 12:41:36 +0400

    feature(deps): add ruduino crate

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,15 @@
 #![no_std]
 #![no_main]
 
-extern crate avr_std_stub;
-
-use avrd::atmega128rfa1::{
-	DDRE,
-	PORTE,
-	SREG,
-	TCCR1A,
-	TCCR1B,
-	OCR1A,
-	TCNT1
-};
-use core::ptr::{read_volatile, write_volatile};
-use avr_delay::delay_ms;
+use ruduino::Pin;
+use ruduino::cores::current::{port};
 
 #[no_mangle]
 pub extern fn main() {
-	unsafe {
-		write_volatile(SREG, !(1 << 7));
-		write_volatile(DDRE, read_volatile(DDRE) | 0b11111111);
-		write_volatile(PORTE, 0);
-
-		write_volatile(TCCR1A, 0);
-
-		loop {
-			write_volatile(PORTE, 0b00000001);
-			delay_ms(1000);
-			write_volatile(PORTE, 0b00000000);
-			delay_ms(1000);
-		}
+	loop {
+		port::B5::set_high();
+		ruduino::delay::delay_ms(1000);
+		port::B5::set_low();
+		ruduino::delay::delay_ms(1000);
 	}
 }
