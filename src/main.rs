3768034ee582ffcbe025595097004fccec9a4b35
NikBel3476 <nikita.belkin3476@gmail.com>
2022-11-17 20:19:41 +0400

    feature: add buttons handling

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,9 @@ const TIM1_PRESCALER: u64 = 1024;
 const INTERRUPT_EVERY_1_HZ_1024_PRESCALER: u16 =
     ((ruduino::config::CPU_FREQUENCY_HZ as f64 / (DESIRED_HZ_TIM1 * TIM1_PRESCALER as f64)) as u64 - 1) as u16;
 
+static mut IS_WINDOW_OPEN: bool = false;
+static mut IS_WINDOW_CLOSE: bool = true;
+
 #[no_mangle]
 pub extern fn main() {
 	without_interrupts(|| {
@@ -40,7 +43,16 @@ pub extern fn main() {
 	port::B5::set_high();
 	port::E0::set_output();
 	port::E0::set_high();
-	// port::E1::set_output();
+
+	// pin for engine control
+	port::E2::set_output();
+	port::E2::set_low();
+
+	//pins for buttons handling
+	port::E3::set_input();
+	port::E4::set_input();
+	port::E5::set_input();
+	port::E6::set_input();
 
 	DDRF::set_mask_raw(0b11111111);
 	PORTF::set_mask_raw(0b0);
@@ -52,6 +64,27 @@ pub extern fn main() {
 		// for &b in b"Hello, from Rust!\n" {
 		// 	serial::transmit(b);
 		// }
+
+		if port::E3::is_high() {
+			port::E2::set_high();
+		}
+
+		if port::E4::is_high() {
+			port::E2::set_low()
+		}
+
+		unsafe {
+			if IS_WINDOW_CLOSE && port::E6::is_high() {
+				IS_WINDOW_CLOSE = false;
+				IS_WINDOW_OPEN = true;
+				port::E2::set_low();
+			}
+			if IS_WINDOW_OPEN && port::E5::is_high() {
+				IS_WINDOW_OPEN = false;
+				IS_WINDOW_CLOSE = true;
+				port::E2::set_low();
+			}
+		}
 
 		// read byte if there is something available
 		if let Some(b) = serial::try_receive() {
