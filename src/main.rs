98e00a8043a20f60bdbada471ccdc298cd259cfa
NikBel3476 <nikita.belkin3476@gmail.com>
2023-06-04 16:10:08 +0400

    refactor: add constants for messages, remove unused comments

--- src/main.rs
+++ src/main.rs
@@ -22,37 +22,34 @@ use ebyte_e32::{
 use embedded_hal::digital::v2::InputPin;
 use nb::block;
 use panic_halt as _;
-// use ruduino::cores::current;
-// use ruduino::cores::current::{port, DDRF, PORTF};
-// use ruduino::interrupt::without_interrupts;
-// use ruduino::legacy::serial;
-// use ruduino::modules::{ClockSource16, Timer16, WaveformGenerationMode16};
-// use ruduino::{Pin, Register};
 
-// uart
-// const BAUD: u32 = 9600;
-// const UBRR: u16 = (ruduino::config::CPU_FREQUENCY_HZ / 16 / BAUD - 1) as u16;
-
-// timer
-// const DESIRED_HZ_TIM1: f64 = 1.0;
-// const TIM1_PRESCALER: u64 = 1024;
-// const INTERRUPT_EVERY_1_HZ_1024_PRESCALER: u16 =
-// 	((ruduino::config::CPU_FREQUENCY_HZ as f64 / (DESIRED_HZ_TIM1 * TIM1_PRESCALER as f64)) as u64
-// 		- 1) as u16;
-
-// enum WINDOW_STATE {
-// 	Open,
-// 	Close,
-// 	Moving
-// }
-
-// struct GlobalState {
-// 	time: u32,
-// 	serial0: *mut Usart<Atmega, USART0, Pin<Input, PE0>, Pin<Output, PE1>, MHz16>
-// }
-
-const MESSAGE_SEPARATOR: u8 = ';' as u8;
+const MESSAGE_SEPARATOR: u8 = b';';
 const SECONDS_IN_DAY: u32 = 24 * 60 * 60;
+
+const OPEN_WINDOW_REQUEST: &[u8; 1] = b"o";
+const CLOSE_WINDOW_REQUEST: &[u8; 1] = b"c";
+const GET_STATE_REQUEST: &[u8; 1] = b"s";
+const GET_GLOBAL_TIME_REQUEST: &[u8; 1] = b"t";
+const UPDATE_GLOBAL_TIME_REQUEST: &[u8; 1] = b"u";
+const SET_TIME_MODE_REQUEST: &[u8; 1] = b"r";
+const ENABLE_SCHEDULE_REQUEST: &[u8; 15] = b"enable_schedule";
+const DISABLE_SCHEDULE_REQUEST: &[u8; 16] = b"disable_schedule";
+const DISABLE_TIME_MODE_REQUEST: &[u8; 1] = b"d";
+const ENABLE_TIME_MODE_REQUEST: &[u8; 1] = b"e";
+const GET_TIME_MODE_RANGE_REQUEST: &[u8; 1] = b"a";
+const SET_SCHEDULE_REQUEST: &[u8; 1] = b"h";
+const GET_SCHEDULE_TIME: &[u8; 1] = b"b";
+
+const WINDOW_OPENED_RESPONSE: u8 = b'o';
+const WINDOW_CLOSED_RESPONSE: u8 = b'c';
+const SET_TIME_OK_RESPONSE: &[u8; 11] = b"set_time_ok";
+const SET_TIME_ERR_RESPONSE: &[u8; 12] = b"set_time_err";
+const SCHEDULE_ENABLED_RESPONSE: &[u8; 16] = b"schedule_enabled";
+const SHCEDULE_ERR_RESPONSE: &[u8; 12] = b"schedule_err";
+const SHCEDULE_DISABLED_RESPONSE: &[u8; 17] = b"schedule_disabled";
+const TIME_MODE_ENABLED_OK_RESPONSE: &[u8; 9] = b"enable_ok";
+const TIME_MODE_ENABLED_ERR_RESPONSE: &[u8; 10] = b"enable_err";
+const TIME_MODE_DISABLED_RESPONSE: &[u8; 10] = b"disable_ok";
 
 enum TimeModeActionState {
 	ShouldBeOpened,
@@ -79,7 +76,6 @@ static mut WINDOW_IS_OPENED: AtomicBool = AtomicBool::new(false);
 static mut WINDOW_IS_CLOSED: AtomicBool = AtomicBool::new(true);
 static mut WINDOW_IS_OPENING: AtomicBool = AtomicBool::new(false);
 static mut WINDOW_IS_CLOSING: AtomicBool = AtomicBool::new(false);
-// static mut LED: mem::MaybeUninit<Pin<Output, port::PB5>> = mem::MaybeUninit::uninit();
 static mut GLOBAL_TIME_IN_SEC: u32 = 0;
 static mut TIME_MODE: TimeMode = TimeMode {
 	active_time_in_sec: 0,
@@ -98,21 +94,6 @@ static mut SCHEDULE: Schedule = Schedule {
 
 #[arduino_hal::entry]
 fn main() -> ! {
-	// without_interrupts(|| {
-	// 	current::Timer16::setup()
-	// 		.waveform_generation_mode(WaveformGenerationMode16::ClearOnTimerMatchOutputCompare)
-	// 		.clock_source(ClockSource16::Prescale1024)
-	// 		.output_compare_1(Some(INTERRUPT_EVERY_1_HZ_1024_PRESCALER))
-	// 		.configure();
-
-	// 	serial::Serial::new(UBRR)
-	// 		.character_size(serial::CharacterSize::EightBits)
-	// 		.mode(serial::Mode::Asynchronous)
-	// 		.parity(serial::Parity::Disabled)
-	// 		.stop_bits(serial::StopBits::OneBit)
-	// 		.configure();
-	// });
-
 	let mut receiving_message: [u8; 50] = [0; 50];
 	let mut receiving_message_position = 0;
 	let mut message_received = false;
@@ -128,67 +109,7 @@ fn main() -> ! {
 		BaudrateExt::into_baudrate(9600),
 	);
 
-	// unsafe {
-	// 	// SAFETY: Interrupts are not enabled at this point so we can safely write the global
-	// 	// variable here.  A memory barrier afterwards ensures the compiler won't reorder this
-	// 	// after any operation that enables interrupts.
-	// 	INTERRUPT_STATE = mem::MaybeUninit::new(InterruptState {
-	// 			time: 0
-	// 	});
-	// 	core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
-	// }
-
 	let timer1 = dp.TC1;
-
-	// let mut serial2 = arduino_hal::Usart::new(
-	// 	dp.USART1,
-	// 	pins.pd2,
-	// 	pins.pd3.into_output(),
-	// 	BaudrateExt::into_baudrate(9600),
-	// );
-
-	// let m0 = pins.pe2.into_output();
-	// let m1 = pins.pe3.into_output();
-	// let aux = pins.pe4.into_floating_input();
-	// let delay_lora = arduino_hal::Delay::new();
-
-	// let mut ebyte = Ebyte::new(serial1, aux, m0, m1, delay_lora).unwrap_or_else(|_| loop {
-	// 	serial2.write_char('e');
-	// });
-
-	// let mut led = pins.pb5.into_output();
-	// led.set_low();
-	// unsafe {
-	// 	LED = mem::MaybeUninit::new(led);
-	// 	core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
-	// }
-
-	// let mut led = pins.pb0.into_output().downgrade();
-	// led.toggle();
-
-	// port::B5::set_output();
-	// port::B5::set_high();
-	// port::E0::set_output();
-	// port::E0::set_high();
-
-	// pin for engine control(relay)
-	// port::E2::set_output();
-	// port::E2::set_low();
-	// TODO: change pin
-	// let mut relay = pins.pe2.into_output();
-	// relay.set_low();
-
-	// TODO: change pins
-	// let enable_engine_button = pins.pe3.into_floating_input();
-	// let disable_engine_button = pins.pe4.into_floating_input();
-
-	// init leds for lora
-	// unsafe {
-	// 	(*arduino_hal::pac::PORTF::PTR)
-	// 		.ddrf
-	// 		.write(|w| w.bits(0b11111111));
-	// 	(*arduino_hal::pac::PORTF::PTR).portf.write(|w| w.bits(0));
-	// }
 
 	// engine rotation direction
 	// low -> close; high -> open
@@ -211,20 +132,6 @@ fn main() -> ! {
 	}
 
 	loop {
-		// serial::transmit(0b00001111);
-
-		// transmitting data via uart
-		// for &b in b"Hello, from Rust!\n" {
-		// 	serial::transmit(b);
-		// }
-
-		// Read a byte from the serial connection
-		// if let Ok(b) = serial1.read() {
-		// 	unsafe {
-		// 		(*arduino_hal::pac::PORTF::PTR).portf.write(|w| w.bits(b));
-		// 	}
-		// PORTF::write(b);
-
 		if let Ok(byte) = serial1.read() {
 			// serial1.write_byte(byte);
 			if !message_received {
@@ -237,7 +144,7 @@ fn main() -> ! {
 						message_received = false;
 					}
 				}
-				if byte as char == ';' {
+				if byte == MESSAGE_SEPARATOR {
 					message_received = true;
 				}
 			}
@@ -250,100 +157,146 @@ fn main() -> ! {
 			{
 				let message = &receiving_message[0..length];
 				unsafe {
-					if message.starts_with("o".as_bytes()) {
+					if message.starts_with(OPEN_WINDOW_REQUEST) {
 						// open window
 						try_open(&mut engine_inverse_pin, &mut engine_direction_pin);
-					} else if message.starts_with("c".as_bytes()) {
+					} else if message.starts_with(CLOSE_WINDOW_REQUEST) {
 						// close window
 						try_close(&mut engine_inverse_pin, &mut engine_direction_pin);
-					} else if message.starts_with("s".as_bytes()) {
+					} else if message.starts_with(GET_STATE_REQUEST) {
 						// get state
-						if WINDOW_IS_OPENED.load(Ordering::SeqCst)
-							&& !WINDOW_IS_CLOSED.load(Ordering::SeqCst)
-						{
-							serial1.write_byte(b'o');
+						if WINDOW_IS_OPENED.load(Ordering::SeqCst) {
+							serial1.write_byte(WINDOW_OPENED_RESPONSE);
 						}
-						if !WINDOW_IS_OPENED.load(Ordering::SeqCst)
-							&& WINDOW_IS_CLOSED.load(Ordering::SeqCst)
-						{
-							serial1.write_byte(b'c');
+						if WINDOW_IS_CLOSED.load(Ordering::SeqCst) {
+							serial1.write_byte(WINDOW_CLOSED_RESPONSE);
 						}
-					} else if message.starts_with("t".as_bytes()) {
+					} else if message.starts_with(GET_GLOBAL_TIME_REQUEST) {
 						// get time
 						serial1.write_byte(b't');
 						for byte in GLOBAL_TIME_IN_SEC.to_be_bytes() {
 							serial1.write_byte(byte);
 						}
-					} else if message.starts_with("u".as_bytes()) {
+					} else if message.starts_with(UPDATE_GLOBAL_TIME_REQUEST) {
 						// update global time
-						GLOBAL_TIME_IN_SEC =
+						let updated_time =
 							u32::from_be_bytes([message[1], message[2], message[3], message[4]]);
-						for &byte in b"ok" {
-							serial1.write_byte(byte);
+						match updated_time.cmp(&SECONDS_IN_DAY) {
+							cmp::Ordering::Less => {
+								GLOBAL_TIME_IN_SEC = updated_time;
+								for &byte in SET_TIME_OK_RESPONSE {
+									serial1.write_byte(byte);
+								}
+							}
+							_ => {
+								for &byte in SET_TIME_ERR_RESPONSE {
+									serial1.write_byte(byte);
+								}
+							}
 						}
-					} else if message.starts_with("r".as_bytes()) {
+					} else if message.starts_with(SET_TIME_MODE_REQUEST) {
 						// set time mode
 						TIME_MODE.active_time_in_sec =
 							u32::from_be_bytes([message[1], message[2], message[3], message[4]]);
 						TIME_MODE.delay_time_in_sec =
 							u32::from_be_bytes([message[5], message[6], message[7], message[8]]);
 						TIME_MODE.enabled = true;
-						for &byte in b"enable_ok" {
+						SCHEDULE.enabled = false;
+						for &byte in TIME_MODE_ENABLED_OK_RESPONSE {
 							serial1.write_byte(byte);
 						}
-					} else if message.starts_with("d".as_bytes()) {
+						delay_ms(10);
+						for &byte in SHCEDULE_DISABLED_RESPONSE {
+							serial1.write_byte(byte);
+						}
+					} else if message.starts_with(DISABLE_SCHEDULE_REQUEST) {
+						SCHEDULE.enabled = false;
+						for &byte in SHCEDULE_DISABLED_RESPONSE {
+							serial1.write_byte(byte);
+						}
+					} else if message.starts_with(ENABLE_SCHEDULE_REQUEST) {
+						match SCHEDULE.open_time.eq(&SCHEDULE.close_time) {
+							true => {
+								for &byte in SHCEDULE_ERR_RESPONSE {
+									serial1.write_byte(byte);
+								}
+							}
+							false => {
+								SCHEDULE.enabled = true;
+								TIME_MODE.enabled = false;
+								for &byte in SCHEDULE_ENABLED_RESPONSE {
+									serial1.write_byte(byte);
+								}
+								delay_ms(10);
+								for &byte in TIME_MODE_DISABLED_RESPONSE {
+									serial1.write_byte(byte);
+								}
+							}
+						}
+					} else if message.starts_with(DISABLE_TIME_MODE_REQUEST) {
 						// disable time mode
 						TIME_MODE.enabled = false;
-						for &byte in b"disable_ok" {
+						for &byte in TIME_MODE_DISABLED_RESPONSE {
 							serial1.write_byte(byte);
 						}
-					} else if message.starts_with("e".as_bytes()) {
+					} else if message.starts_with(ENABLE_TIME_MODE_REQUEST) {
 						// enable time mode
 						match TIME_MODE.active_time_in_sec > 0 && TIME_MODE.active_time_in_sec > 0 {
 							true => {
 								TIME_MODE.enabled = true;
 								SCHEDULE.enabled = false;
-								for &byte in b"enable_ok" {
+								for &byte in TIME_MODE_ENABLED_OK_RESPONSE {
+									serial1.write_byte(byte);
+								}
+								delay_ms(10);
+								for &byte in SHCEDULE_DISABLED_RESPONSE {
 									serial1.write_byte(byte);
 								}
 							}
 							false => {
-								for &byte in b"enable_err" {
+								for &byte in TIME_MODE_ENABLED_ERR_RESPONSE {
 									serial1.write_byte(byte);
 								}
 							}
 						}
-					} else if message.starts_with("a".as_bytes()) {
-						// get range time
+					} else if message.starts_with(GET_TIME_MODE_RANGE_REQUEST) {
+						// get time range
 						for byte in TIME_MODE.active_time_in_sec.to_be_bytes() {
 							serial1.write_byte(byte);
 						}
 						for byte in TIME_MODE.delay_time_in_sec.to_be_bytes() {
 							serial1.write_byte(byte);
 						}
-					} else if message.starts_with(b"h") {
-						// set time mode
+					} else if message.starts_with(SET_SCHEDULE_REQUEST) {
+						// set schedule
 						let open_time =
 							u32::from_be_bytes([message[1], message[2], message[3], message[4]]);
 						let close_time =
 							u32::from_be_bytes([message[5], message[6], message[7], message[8]]);
-						match open_time < SECONDS_IN_DAY - 1 && close_time < SECONDS_IN_DAY - 1 {
+						match open_time < SECONDS_IN_DAY - 1
+							&& close_time < SECONDS_IN_DAY - 1
+							&& !open_time.eq(&close_time)
+						{
 							true => {
 								SCHEDULE.open_time = open_time;
 								SCHEDULE.close_time = close_time;
 								SCHEDULE.enabled = true;
 								TIME_MODE.enabled = false;
-								for &byte in b"schedule_ok" {
+								for &byte in SCHEDULE_ENABLED_RESPONSE {
+									serial1.write_byte(byte);
+								}
+								delay_ms(10);
+								for &byte in TIME_MODE_DISABLED_RESPONSE {
 									serial1.write_byte(byte);
 								}
 							}
 							false => {
-								for &byte in b"schedule_err" {
+								for &byte in SHCEDULE_ERR_RESPONSE {
 									serial1.write_byte(byte);
 								}
 							}
 						}
-					} else if message.starts_with(b"b") {
+					} else if message.starts_with(GET_SCHEDULE_TIME) {
 						// get schedule state
 						for byte in SCHEDULE.open_time.to_be_bytes() {
 							serial1.write_byte(byte);
@@ -358,83 +311,6 @@ fn main() -> ! {
 			receiving_message_position = 0;
 			message_received = false;
 		}
-
-		// delay_ms(1000);
-		// match ebyte.model_data() {
-		// 	Ok(model_data) => {
-		// 		ufmt::uwriteln!(
-		// 			&mut serial2,
-		// 			"{},{},{}\r",
-		// 			model_data.model,
-		// 			model_data.version,
-		// 			model_data.features
-		// 		)
-		// 		.void_unwrap();
-		// 	}
-		// 	Err(_) => {
-		// 		serial2.write_char('e');
-		// 	}
-		// };
-
-		// delay_ms(1000);
-		// match ebyte.parameters() {
-		// 	Ok(params) => {
-		// 		let bytes = params.to_bytes();
-		// 		ufmt::uwriteln!(
-		// 			&mut serial2,
-		// 			"{},{},{},{},{}\r",
-		// 			bytes[0],
-		// 			bytes[1],
-		// 			bytes[2],
-		// 			bytes[3],
-		// 			bytes[4],
-		// 		)
-		// 		.void_unwrap();
-		// 	}
-		// 	Err(_) => {
-		// 		serial2.write_char('e');
-		// 	}
-		// }
-
-		// if let Ok(byte) = serial2.read() {
-		// 	match byte.to_ascii_lowercase() as char {
-		// 		'm' => {
-		// 			let model_data = ebyte.model_data().unwrap_or_else(|e| {
-		// 			match e {
-		// 				ebyte_e32::Error::SerialWrite => {
-		// 					block!(serial2.write('w' as u8)).unwrap();
-		// 				}
-		// 				ebyte_e32::Error::SerialRead => {
-		// 					block!(serial2.write('r' as u8)).unwrap();
-		// 				}
-		// 				ebyte_e32::Error::ReadModelData => {
-		// 					block!(serial2.write('n' as u8)).unwrap();
-		// 				}
-		// 				_ => {
-		// 					block!(serial2.write('e' as u8)).unwrap();
-		// 				}
-		// 			}
-		// 			loop {
-		// 				block!(serial2.write('e' as u8)).unwrap();
-		// 			}
-		// 		});
-
-		// 		block!(serial2.write(model_data.model)).unwrap();
-		// 		block!(serial2.write(model_data.version)).unwrap();
-		// 		block!(serial2.write(model_data.features)).unwrap();
-		// 		},
-		// 		'p' => {},
-		// 		_ => {}
-		// 	}
-		// };
-
-		// if enable_engine_button.is_high() {
-		// 	relay.set_high();
-		// }
-
-		// if disable_engine_button.is_high() {
-		// 	relay.set_low();
-		// }
 
 		unsafe {
 			if TIME_MODE.enabled {
@@ -479,15 +355,10 @@ fn main() -> ! {
 				}
 			}
 		}
-
-		// read byte if there is something available
-		// if let Some(b) = serial::try_receive() {
-		// 	PORTF::write(0b0);
-		// 	PORTF::set_mask_raw(b);
-		// }
 	}
 }
 
+#[inline]
 pub fn try_open<P1, P2>(
 	engine_inverse_pin: &mut Pin<Output, P1>,
 	engine_direction_pin: &mut Pin<Output, P2>,
@@ -504,6 +375,7 @@ pub fn try_open<P1, P2>(
 	}
 }
 
+#[inline]
 pub fn try_close<P1, P2>(
 	engine_inverse_pin: &mut Pin<Output, P1>,
 	engine_direction_pin: &mut Pin<Output, P2>,
@@ -560,13 +432,14 @@ pub fn reg_timer(timer1: &TC1) {
 	timer1.timsk1.write(|w| w.ocie1a().set_bit()); //enable this specific interrupt
 }
 
-#[avr_device::interrupt(atmega328p)]
+#[avr_device::interrupt(atmega128rfa1)]
 fn TIMER1_COMPA() {
 	unsafe {
 		GLOBAL_TIME_IN_SEC = match GLOBAL_TIME_IN_SEC.cmp(&(SECONDS_IN_DAY - 1)) {
 			cmp::Ordering::Less => GLOBAL_TIME_IN_SEC + 1,
 			_ => 0,
 		};
+
 		if TIME_MODE.enabled {
 			match TIME_MODE.action_state {
 				TimeModeActionState::ShouldBeOpened => {
@@ -595,29 +468,28 @@ fn TIMER1_COMPA() {
 				}
 			}
 		}
+
 		if SCHEDULE.enabled {
-			SCHEDULE.action_state = match GLOBAL_TIME_IN_SEC.cmp(&SCHEDULE.open_time)
-				== cmp::Ordering::Greater
-				&& GLOBAL_TIME_IN_SEC.cmp(&SCHEDULE.close_time) == cmp::Ordering::Less
-			{
-				true => TimeModeActionState::ShouldBeOpened,
-				false => TimeModeActionState::ShouldBeClosed,
+			match SCHEDULE.open_time.cmp(&SCHEDULE.close_time) {
+				cmp::Ordering::Less => {
+					SCHEDULE.action_state = match GLOBAL_TIME_IN_SEC >= SCHEDULE.open_time
+						&& GLOBAL_TIME_IN_SEC <= SCHEDULE.close_time
+					{
+						true => TimeModeActionState::ShouldBeOpened,
+						false => TimeModeActionState::ShouldBeClosed,
+					}
+				}
+				_ => {
+					SCHEDULE.action_state = match GLOBAL_TIME_IN_SEC >= SCHEDULE.open_time
+						|| GLOBAL_TIME_IN_SEC <= SCHEDULE.close_time
+					{
+						true => TimeModeActionState::ShouldBeOpened,
+						false => TimeModeActionState::ShouldBeClosed,
+					}
+				}
 			}
 		}
+
 		(*atmega_hal::pac::TC1::PTR).tcnt1.write(|w| w.bits(0))
 	}
 }
-
-// #[no_mangle]
-// pub unsafe extern "avr-interrupt" fn __vector_17() {
-// 	// port::B5::toggle();
-// 	// port::E0::toggle();
-// 	// (*arduino_hal::pac::USART0::PTR).udr0.as_ptr()
-// }
-
-// #[avr_device::interrupt(atmega128rfa1)]
-// fn USART1_TX() {
-// 	let led = unsafe { &mut *LED.as_mut_ptr() };
-
-// 	led.toggle();
-// }
